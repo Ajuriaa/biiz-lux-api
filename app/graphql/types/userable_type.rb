3b7d020d07ea2bb4236833009a42813740091dd6
Ajuriaa <alejoajuria@gmail.com>
2023-06-27 01:00:02 -0600

    linting issues fixed

--- app/graphql/types/userable_type.rb
+++ app/graphql/types/userable_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :score, Integer, null: true
     field :license, String, null: true
-    field :license_expiration_date, String, null:true
-    field :payment_method, String, null:true
+    field :license_expiration_date, String, null: true
+    field :payment_method, String, null: true
   end
 end
