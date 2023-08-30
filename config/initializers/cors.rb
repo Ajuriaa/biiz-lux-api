b6c9c744034a5644f5160d683d2c034df157a30f
Ajuriaa <alejoajuria@gmail.com>
2023-08-30 13:34:56 -0600

    127.0.0.1 added to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /\Ahttp:\/\/localhost(:\d+)?\z/
+    origins /\Ahttp:\/\/localhost(:\d+)?\z/, /\Ahttp:\/\/127\.0\.0\.1(:\d+)?\z/
 
     resource '*',
       headers: :any,
