43a88d0171de21732e70d3f36d1c3fc100a11d98
Ajuuu <alejoajuria@gmail.com>
2023-09-27 01:53:12 -0600

    webSocket configured for rails

---
+++ app/channels/driver_coordinates_channel.rb
@@ -0,0 +1,18 @@
+class DriverCoordinatesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'driver_coordinates_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def get_driver_coordinates
+    ActionCable.server.broadcast('driver_coordinates_channel', 'sendCoordinates')
+  end
+
+  def send_coordinates(data)
+    coords = data['coords']
+    ActionCable.server.broadcast('driver_coordinates_channel', coords)
+  end
+end
