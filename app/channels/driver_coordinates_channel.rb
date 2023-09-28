b3d89d6a662d62a4c856d2132d6e4bc9728a8d60
Ajuuu <alejoajuria@gmail.com>
2023-09-28 15:51:56 -0600

    new travel method added

--- app/channels/driver_coordinates_channel.rb
+++ app/channels/driver_coordinates_channel.rb
@@ -15,4 +15,9 @@ class DriverCoordinatesChannel < ApplicationCable::Channel
     coords = data['coords']
     ActionCable.server.broadcast('driver_coordinates_channel', coords)
   end
+
+  def new_travel(data)
+    travel_info = data['info']
+    ActionCable.server.broadcast('driver_coordinates_channel', travel_info)
+  end
 end
